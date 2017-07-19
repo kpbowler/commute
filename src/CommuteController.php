316f47368b50ab5e3861fb0132c778789a74182c
Kevin Bowler <kpbowler@gmail.com>
2017-07-19 16:24:37 +0100

    Adding some code!

---
+++ src/CommuteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kpbowler\Commute;
+
+use App\Http\Controllers\Controller;
+
+class CommuteController extends Controller
+{
+    public function deploy()
+    {
+        echo 'boo';
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('api/commute/deploy',
+'kpbowler\commute\CommuteController@deploy');
